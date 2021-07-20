3b2dbd4e84ece1f22c8728e7c5fb242001e68c4d
Sai Thu Ya Aung <saithuyaaung1@gmail.com>
2021-07-21 04:37:42 +0700

    update

---
+++ plot1.R
@@ -0,0 +1,19 @@
+## Read dataset and put into "data" variable
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+## Set Date format
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+## Only select data from date 2007-02-01 to 2007-02-02
+selected_data <- data[which(data$Date == "2007-02-01" | data$Date == "2007-02-02"),]
+
+## Set Global_active_power variable to numeric 
+selected_data$Global_active_power <- as.numeric(as.character(selected_data$Global_active_power))
+
+## create png grphic device
+png("plot1.png", width = 480, height = 480)
+
+## plot
+hist(selected_data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,36 @@
+## Read dataset and put into "data" variable
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+## Set Date format
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$Time <- format(data$Time, format = "%H:%M:%S")
+
+## Only select data from date 2007-02-01 to 2007-02-02
+selected_data <- data[which(data$Date == "2007-02-01" | data$Date == "2007-02-02"),]
+
+## Set new Date and Time variable
+newDateTime <- paste(selected_data$Date, selected_data$Time)
+newDateTime <- strptime(newDateTime, format = "%Y-%m-%d %H:%M:%S")
+
+## Set Sub_metering variables to numeric 
+selected_data$Global_active_power <- as.numeric(as.character(selected_data$Global_active_power))
+selected_data$Voltage <- as.numeric(as.character(selected_data$Voltage))
+selected_data$Sub_metering_1 <- as.numeric(as.character(selected_data$Sub_metering_1))
+selected_data$Sub_metering_2 <- as.numeric(as.character(selected_data$Sub_metering_2))
+selected_data$Sub_metering_3 <- as.numeric(as.character(selected_data$Sub_metering_3))
+selected_data$Global_reactive_power<- as.numeric(as.character(selected_data$Global_reactive_power))
+
+## create png grphic device
+png("plot4.png", width = 480, height = 480)
+
+## plot
+par(mfrow = c(2,2))
+plot(newDateTime, selected_data$Global_active_power, type = "l",xlab = "", ylab = "Global Active Power")
+plot(newDateTime, selected_data$Voltage, type = "l",xlab = "datetime", ylab = "Voltage")
+plot(newDateTime, selected_data$Sub_metering_1, type = "l",xlab = "", ylab = "Energy sub metering")
+lines(newDateTime, selected_data$Sub_metering_2, type = "l",xlab = "", col = "red")
+lines(newDateTime, selected_data$Sub_metering_3, type = "l",xlab = "", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, bty = "n")
+plot(newDateTime, selected_data$Global_reactive_power, type = "l",xlab = "", ylab = "Global_reactive_power")
+
+dev.off()
